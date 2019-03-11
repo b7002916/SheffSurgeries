541a938da281703aebf20e56ecd5b4eb1e6bb56b
b7002916 <b7002916@my.shu.ac.uk>
2019-03-11 12:20:03 +0000

    In this stage, I have gone into Patient.groovy and declared each variable with a specific datatype

--- appointmentSystem/grails-app/domain/com/latack/Patient.groovy
+++ appointmentSystem/grails-app/domain/com/latack/Patient.groovy
@@ -2,6 +2,14 @@ package com.latack
 
 class Patient {
 
+String patientName
+String patientAddress
+String patientResidence
+Date patientDob
+String patientID
+Date dateRegistered
+int patientPhone
+
     static constraints = {
     }
 }
