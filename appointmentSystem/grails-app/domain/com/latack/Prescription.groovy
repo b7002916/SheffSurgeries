b5e5e9c84eddc13cd0e0e9e646aa701c5d034a62
b7002916 <b7002916@my.shu.ac.uk>
2019-03-11 14:12:42 +0000

    Here I have added constraints for each variable within Prescription.groovy

--- appointmentSystem/grails-app/domain/com/latack/Prescription.groovy
+++ appointmentSystem/grails-app/domain/com/latack/Prescription.groovy
@@ -14,5 +14,13 @@ Boolean patientPaying
 
 
     static constraints = {
+
+pharmacyName blank: false, nullable: false, maxSize: 50
+prescripNumber blank: false, nullable: false, max: 5, unique: true
+medicine blank: false, nullable: false 
+totalCost blank: false, nullable: false, scale: 2
+dateIssued blank: false, nullable: false
+patientPaying blank: false, nullable: false
+
     }
 }
