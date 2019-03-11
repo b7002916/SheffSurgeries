f1293f1b6f5859d8cb14be579d71cf4ebf98d839
b7002916 <b7002916@my.shu.ac.uk>
2019-03-11 12:13:35 +0000

    In this stage, I have gone into Surgery.groovy and declared each variable with a specific datatype

--- appointmentSystem/grails-app/domain/com/latack/Surgery.groovy
+++ appointmentSystem/grails-app/domain/com/latack/Surgery.groovy
@@ -2,6 +2,15 @@ package com.latack
 
 class Surgery {
 
+String name
+String address
+String postcode
+int telephone
+int numberOfPatients
+String description
+double openingTime
+
+
     static constraints = {
     }
 }
