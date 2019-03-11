ca1aa4c28f269850e0159bd7a8ac87a234022102
b7002916 <b7002916@my.shu.ac.uk>
2019-03-11 14:50:13 +0000

    Here I have added constraints for each variable within Receptionist.groovy

--- appointmentSystem/grails-app/domain/com/latack/Receptionist.groovy
+++ appointmentSystem/grails-app/domain/com/latack/Receptionist.groovy
@@ -9,5 +9,12 @@ String recepPassword
 int recepPhone
 
     static constraints = {
+
+recepName blank: false, nullable: false, maxSize: 25
+recepEmail blank: false, nullable: false, email: true
+recepUsername blank: false, nullable: false, unique: true
+recepPassword blank: false, nullable: false, minSize: 6
+recepPhone blank: false, nullable: false, min: 11, max: 11
+
     }
 }
