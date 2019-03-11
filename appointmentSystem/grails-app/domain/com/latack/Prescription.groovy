6951a41b992d34060067720229fc9f6e82c4d825
b7002916 <b7002916@my.shu.ac.uk>
2019-03-11 12:10:23 +0000

    In this stage, I have gone into Prescription.groovy and declared each variable with a specific datatype

--- appointmentSystem/grails-app/domain/com/latack/Prescription.groovy
+++ appointmentSystem/grails-app/domain/com/latack/Prescription.groovy
@@ -2,6 +2,17 @@ package com.latack
 
 class Prescription {
 
+String pharmacyName
+int prescripNumber
+String medicine
+float totalCost
+Date dateIssued
+Boolean patientPaying
+
+
+
+
+
     static constraints = {
     }
 }
