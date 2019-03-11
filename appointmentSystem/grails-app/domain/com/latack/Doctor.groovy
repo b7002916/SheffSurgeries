3170154cfe79158762e8d23e43852ce8e57cd77c
b7002916 <b7002916@my.shu.ac.uk>
2019-03-11 14:37:49 +0000

    Here I have added constraints for each variable within Doctor.groovy

--- appointmentSystem/grails-app/domain/com/latack/Doctor.groovy
+++ appointmentSystem/grails-app/domain/com/latack/Doctor.groovy
@@ -12,5 +12,15 @@ int doctorPhone
 String bio
 
     static constraints = {
+
+fullName blank: false, nullable: false, maxSize: 30
+qualification blank: false, nullable: false, widget: 'Qualifications'
+position blank: false, nullable: false, maxSize: 20
+doctorEmail blank: false, nullable: false, email: true
+password blank: false, nullable: false, minSize: 5
+doctorOffice blank: false, nullable: false, size: 6..6
+doctorPhone blank: false, nullable: false, min: 11, max: 11
+bio widget: 'Bio'
+
     }
 }
