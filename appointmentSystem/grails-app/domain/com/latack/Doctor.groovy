89e4d2b7eb1d470d5857cfe7c0647213e7e4d432
b7002916 <b7002916@my.shu.ac.uk>
2019-03-12 15:00:17 +0000

    This stage I can now display 2 instances of the domain class Doctor

--- appointmentSystem/grails-app/domain/com/latack/Doctor.groovy
+++ appointmentSystem/grails-app/domain/com/latack/Doctor.groovy
@@ -8,19 +8,19 @@ String position
 String doctorEmail 
 String password
 String doctorOffice
-int doctorPhone
+String doctorPhone
 String bio
 
     static constraints = {
 
 fullName blank: false, nullable: false, maxSize: 30
-qualification blank: false, nullable: false, widget: 'Qualifications', maxSize: 100
+qualification blank: false, nullable: false, maxSize: 100
 position blank: false, nullable: false, maxSize: 20
 doctorEmail blank: false, nullable: false, email: true
 password blank: false, nullable: false, minSize: 5
 doctorOffice blank: false, nullable: false, size: 6..6
 doctorPhone blank: false, nullable: false, size: 11..11
-bio widget: 'Bio', maxSize: 100
+bio maxSize: 100
 
     }
 }
--- appointmentSystem/grails-app/init/appointmentsystem/BootStrap.groovy
+++ appointmentSystem/grails-app/init/appointmentsystem/BootStrap.groovy
@@ -24,6 +24,32 @@ nursePhone: '01142224433'
 
 ).save()
 
+def SarahMcdonald = new Doctor (
+
+fullName: 'Dr Sarah Macdonald',
+qualification: 'MBChB(Sheffield)',
+position: 'GP, Surgeon',
+doctorEmail: 's.macdonald@myemail.com',
+password: 'secret222',
+doctorOffice: 'D-9888',
+doctorPhone: '01477684089',
+bio: 'Lorem accumsan.'
+
+).save()
+
+def JackMcgue = new Doctor (
+
+fullName: 'Dr Jack Mcgue',
+qualification: 'pHD Pharmeutical Science(Sheffield)',
+position: 'GP, Surgeon',
+doctorEmail: 'j.mcgue@myemail.com',
+password: 'secret255',
+doctorOffice: 'A-7688',
+doctorPhone: '01477684088',
+bio: 'Lorem accumsan.'
+
+).save()
+
     }
     def destroy = {
     }
